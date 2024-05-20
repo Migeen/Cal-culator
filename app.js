67ef959b38620aa38eabaa090556648697b21763
Migeen Lamichhane Magar <migeen999@gmail.com>
2024-05-20 20:32:02 +0545

    calculator succesfully completed

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const display = document.querySelector('.display');
 const clearBtn = document.querySelector('#ac'); 
 const delBtn = document.querySelector('#del');
 const opBtns = document.querySelectorAll('#operator');
+const equals = document.querySelector('#equals'); 
+
 let currentInput = '';
 let previousInput = '';
 let operator = null;
@@ -51,6 +53,7 @@ const chooseOperator = (newOperator) =>{
     operator = newOperator;
     shouldResetDisp = true;
     calcFinished = false;
+    // console.log(newOperator);
 }
 
 let sum =function(a,b){
@@ -72,16 +75,32 @@ let divide = function(a,b){
     return a/b;
 }
 
-let operate = (x,y,operator)=>{
+let operate = ()=>{
+    let result=0;
+    const a = parseFloat(previousInput);
+    const b = parseFloat(currentInput);
+
+    // console.log(a);
+    // console.log(b);
+    // console.log(operator);
+
     if(operator== '+'){
-        return sum(x,y);
+        result =  a+b;
     }else if(operator== '-'){
-        return subtract(x,y);
+        result =  subtract(a,b);
     }else if(operator== '*'){
-        return multiply(x,y);
+        result = multiply(a,b);
     }else if(operator=='/'){
-        return divide(x,y);
+        result = divide(a,b);
     }
+
+    currentInput = result.toString();
+    shouldResetDisp = true;
+    calcFinished = true;
+    operator = null;
+    previousInput ='';
+
+    updateDisplay(currentInput); 
 }
 
 btns.forEach((btn)=>{
@@ -93,3 +112,11 @@ btns.forEach((btn)=>{
 
 clearBtn.addEventListener('click',clear);
 delBtn.addEventListener('click',deleteNum);
+
+opBtns.forEach((btn) =>{
+    btn.addEventListener('click',() => chooseOperator(btn.textContent));
+});
+
+equals.addEventListener('click', ()=>{
+    operate();
+}); 
